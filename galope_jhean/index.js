daa7181467e119901ae6f2a2e4e62bc2fcfa2ffc
Jhean Khendrick <drakekhendrick@gmail.com>
2023-11-04 17:34:01 +0800

    add comment functionality

--- galope_jhean/index.js
+++ galope_jhean/index.js
@@ -1,6 +1,7 @@
 const commentBtn = document.querySelector(".comment-btn");
 const nameBox = document.querySelector("#input_name");
 const commentBox = document.querySelector("#input_comment");
+const commentContainer = document.querySelector(".comment-container");
 
 const handleInputChange = () => {
     const nameLength = nameBox.value.length;
@@ -8,5 +9,31 @@ const handleInputChange = () => {
     commentBtn.disabled = !(commentLength > 1 && nameLength > 1);
 };
 
+const addComment = () => {
+    const name = nameBox.value;
+    const comment = commentBox.value;
+
+    if (name && comment) {
+        const newComment = document.createElement("div");
+        newComment.classList.add("main-comment");
+        newComment.innerHTML = `
+            <div class="icon">
+                <img src="assets/avatar.webp" alt="">
+            </div>
+            <div class="details">
+                <h4>${name}</h4>
+                <p>${comment}</p>
+            </div>
+        `;
+        commentContainer.appendChild(newComment);
+        nameBox.value = "";
+        commentBox.value = "";
+        commentBtn.disabled = true;
+
+        window.scrollTo(0, document.body.scrollHeight);
+    }
+};
+
+commentBtn.addEventListener("click", addComment);
 commentBox.addEventListener('input', handleInputChange);
 nameBox.addEventListener('input', handleInputChange);
