bc00b19f25ce42e8f00290ae08250026ba58dce5
Mark_Ryan <odruniamarkryan@gmail.com>
2023-11-04 16:32:22 +0800

    change to query selector in js

--- Odrunia_Mark_Ryan/scrpts.js
+++ Odrunia_Mark_Ryan/scrpts.js
@@ -1,11 +1,11 @@
 function commntbtn() {
-    let name = document.getElementById("full_name")
-    let txtarea = document.getElementById("story")
+    let name = document.querySelector("#full_name")
+    let txtarea = document.querySelector("#story")
 
     if (name.value.length > 0 && txtarea.value.length > 0){
-        document.getElementById("btn_comm").disabled = false
+        document.querySelector("#btn_comm").disabled = false
     }
     else{
-        document.getElementById("btn_comm").disabled = true
+        document.querySelector("#btn_comm").disabled = true
     }
 }
