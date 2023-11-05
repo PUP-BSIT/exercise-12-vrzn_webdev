b9aac28aa617e655144daa6862eb8069b4a5987d
Andronicus Dimasacat <dmsctndrncs@gmail.com>
2023-11-05 13:48:55 +0800

    Shorten the codes in JS

--- dimasacat_andronicus/dimasacat_andronicus.js
+++ dimasacat_andronicus/dimasacat_andronicus.js
@@ -2,29 +2,23 @@ function validate() {
     let name = document.querySelector("#input_name").value.trim();
     let comment = document.querySelector("#input_comment").value.trim();
     let commentButton = document.querySelector("#btn");
-
-    if (name.length > 0 && comment.length > 0) {
-      commentButton.disabled = false;
-    } else {
-      commentButton.disabled = true;
-    }
+    commentButton.disabled = !(name.length > 0 && comment.length > 0);
 }
 
 function addComment() {
     let name = document.querySelector("#input_name").value;
     let comment = document.querySelector("#input_comment").value;
 
-    if (name && comment) {
-      let commentSection = document.querySelector(".guide-comment");
-      let newComment = document.createElement("div");
-      newComment.classList.add("comment-style");
-      newComment.innerHTML = `
+    if (!name || !comment) return;  
+    const commentSection = document.querySelector(".guide-comment");
+    commentSection.appendChild(Object.assign(document.createElement("div"), {
+      className: "comment-style",
+      innerHTML: `
         <p class="comment-name">${name}</p>
-        <p class="comments">${comment}</p>`;
-      commentSection.appendChild(newComment);
-
-      document.querySelector("#input_name").value = "";
-      document.querySelector("#input_comment").value = "";
-      document.querySelector("#btn").disabled = true;
-    }
+        <p class="comments">${comment}</p>`
+    }));
+    
+    document.querySelector("#input_name").value = "";
+    document.querySelector("#input_comment").value = "";
+    document.querySelector("#btn").disabled = true;
 }
