29b958fd1c46d5082095164245632a5fc8d91225
Andronicus Dimasacat <dmsctndrncs@gmail.com>
2023-11-05 12:21:43 +0800

    Add and Update functions in JS

--- dimasacat_andronicus/dimasacat_andronicus.js
+++ dimasacat_andronicus/dimasacat_andronicus.js
@@ -1,10 +1,30 @@
-function func(){
-    let inputname = document.getElementById("input_name");
-    let inputcomment = document.getElementById("input_comment");
-    if(inputname.value.length > 0 && inputcomment.value.length > 0){
-        document.getElementById("btn").disabled = false;
+function validate() {
+    let name = document.querySelector("#input_name").value.trim();
+    let comment = document.querySelector("#input_comment").value.trim();
+    let commentButton = document.querySelector("#btn");
+
+    if (name.length > 0 && comment.length > 0) {
+      commentButton.disabled = false;
+    } else {
+      commentButton.disabled = true;
     }
-    else{
-        document.getElementById("btn").disabled = true;
+}
+
+function addComment() {
+    let name = document.querySelector("#input_name").value;
+    let comment = document.querySelector("#input_comment").value;
+
+    if (name && comment) {
+      let commentSection = document.querySelector(".guide-comment");
+      let newComment = document.createElement("div");
+      newComment.classList.add("comment-style");
+      newComment.innerHTML = `
+        <p class="comment-name">${name}</p>
+        <p class="comments">${comment}</p>`;
+      commentSection.appendChild(newComment);
+
+      document.querySelector("#input_name").value = "";
+      document.querySelector("#input_comment").value = "";
+      document.querySelector("#btn").disabled = true;
     }
 }
